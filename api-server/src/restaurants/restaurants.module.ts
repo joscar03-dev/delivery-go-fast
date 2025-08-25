1abcd319df67635029c437e8ba2fdfb2ef5bedce
José Carlos Pizango Camán <locomancocapac@gmail.com>
2025-08-25 18:36:10 -0500

    agregamos al module del restaurante la categoria de menu y de restaurante

--- api-server/src/restaurants/restaurants.module.ts
+++ api-server/src/restaurants/restaurants.module.ts
@@ -2,14 +2,38 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Restaurant } from './entities/restaurant.entity';
 import { MenuItem } from './entities/menu-item.entity';
-import { Category } from './entities/category.entity';
+import { RestaurantCategory } from './entities/restaurant-category.entity';
+import { MenuCategory } from './entities/menu-category.entity';
 import { RestaurantsService } from './restaurants.service';
 import { RestaurantsController } from './restaurants.controller';
+import { RestaurantCategoriesService } from './services/restaurant-categories.service';
+import { RestaurantCategoriesController } from './controllers/restaurant-categories.controller';
+import { MenuCategoriesService } from './services/menu-categories.service';
+import { MenuCategoriesController } from './controllers/menu-categories.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Restaurant, MenuItem, Category])],
-  controllers: [RestaurantsController],
-  providers: [RestaurantsService],
-  exports: [RestaurantsService],
+  imports: [
+    TypeOrmModule.forFeature([
+      Restaurant,
+      MenuItem,
+      RestaurantCategory,
+      MenuCategory,
+    ]),
+  ],
+  controllers: [
+    RestaurantsController,
+    RestaurantCategoriesController,
+    MenuCategoriesController,
+  ],
+  providers: [
+    RestaurantsService,
+    RestaurantCategoriesService,
+    MenuCategoriesService,
+  ],
+  exports: [
+    RestaurantsService,
+    RestaurantCategoriesService,
+    MenuCategoriesService,
+  ],
 })
 export class RestaurantsModule {}
