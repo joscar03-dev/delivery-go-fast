133c81a337251f04f16c99075ab42428b241de41
José Carlos Pizango Camán <locomancocapac@gmail.com>
2025-08-25 10:58:07 -0500

    agregamos importd, controllers, prtoviders y exports

--- api-server/src/restaurants/restaurants.module.ts
+++ api-server/src/restaurants/restaurants.module.ts
@@ -3,7 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Restaurant } from './entities/restaurant.entity';
 import { MenuItem } from './entities/menu-item.entity';
 import { Category } from './entities/category.entity';
+import { RestaurantsService } from './restaurants.service';
+import { RestaurantsController } from './restaurants.controller';
+
 @Module({
   imports: [TypeOrmModule.forFeature([Restaurant, MenuItem, Category])],
+  controllers: [RestaurantsController],
+  providers: [RestaurantsService],
+  exports: [RestaurantsService],
 })
 export class RestaurantsModule {}
