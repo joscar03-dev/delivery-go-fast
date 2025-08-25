e414938251259dcae93a6ca941f3acdf508bc279
José Carlos Pizango Camán <locomancocapac@gmail.com>
2025-08-25 10:53:17 -0500

    actualizar el menu de restaurante

---
+++ api-server/src/restaurants/dto/update-menu-item.dto.ts
@@ -0,0 +1,27 @@
+import { IsString, IsOptional, IsUUID, IsDecimal } from 'class-validator';
+
+export class UpdateMenuItemDto {
+  @IsString()
+  @IsOptional()
+  name?: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsDecimal()
+  @IsOptional()
+  price?: number;
+
+  @IsString()
+  @IsOptional()
+  imageUrl?: string;
+
+  @IsUUID()
+  @IsOptional()
+  restaurantId?: string;
+
+  @IsUUID()
+  @IsOptional()
+  categoryId?: string;
+}
