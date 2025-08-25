65c8dd7bdc8068e090606c0d2aa48f9a2deb7792
José Carlos Pizango Camán <locomancocapac@gmail.com>
2025-08-25 12:23:02 -0500

    agregamos el role al app module

--- api-server/src/users/users.module.ts
+++ api-server/src/users/users.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Address } from './entities/address.entity';
+import { Role } from '../auth/entities/role.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Address])],
+  imports: [TypeOrmModule.forFeature([User, Address, Role])],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService], // Exportamos el servicio para que pueda ser usado en otros módulos
