f75e777e931195852d0c1a6ff83d529a56c18dff
José Carlos Pizango Camán <locomancocapac@gmail.com>
2025-08-25 20:51:25 -0500

    agregamos al module controllers: [OrdersController],   providers: [OrdersService],   exports: [OrdersService],

--- api-server/src/orders/orders.module.ts
+++ api-server/src/orders/orders.module.ts
@@ -3,8 +3,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from './entities/order.entity';
 import { OrderItem } from './entities/order-item.entity';
 import { Review } from './entities/review.entity';
+import { MenuItem } from '../restaurants/entities/menu-item.entity';
+import { Restaurant } from '../restaurants/entities/restaurant.entity';
+import { OrdersService } from './orders.service';
+import { OrdersController } from './orders.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, OrderItem, Review])],
+  imports: [
+    TypeOrmModule.forFeature([Order, OrderItem, Review, MenuItem, Restaurant]),
+  ],
+  controllers: [OrdersController],
+  providers: [OrdersService],
+  exports: [OrdersService],
 })
 export class OrdersModule {}
