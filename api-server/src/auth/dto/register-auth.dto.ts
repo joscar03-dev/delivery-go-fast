160b182705c75619912e8b166b38d3809491c5f2
José Carlos Pizango Camán <locomancocapac@gmail.com>
2025-09-26 02:20:37 -0500

    modificamos el dto para que se requiera o opcional el rol

--- api-server/src/auth/dto/register-auth.dto.ts
+++ api-server/src/auth/dto/register-auth.dto.ts
@@ -1,6 +1,31 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+  IsOptional,
+  IsEnum,
+} from 'class-validator';
+import { Role } from '../../common/enums/role.enum';
+
 export class RegisterAuthDto {
-  @IsNotEmpty() @IsString() name: string;
-  @IsNotEmpty() @IsEmail() email: string;
-  @IsNotEmpty() @IsString() @MinLength(6) password: string;
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @IsOptional()
+  @IsEnum(Role, {
+    message:
+      'Role must be one of: client, driver, restaurant_owner, super_admin',
+  })
+  role?: Role;
 }
