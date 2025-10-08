0de071657e8b4807fd06061c9dcb8304a01d7514
José Carlos Pizango Camán <locomancocapac@gmail.com>
2025-10-08 02:32:19 -0500

    GeolocationModule - Módulo completo con todas las dependencias

---
+++ api-server/src/geolocation/geolocation.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GeolocationService } from './geolocation.service';
+import { DeliveryGateway } from './delivery.gateway';
+import { DriverLocation } from './entities/driver-location.entity';
+import { User } from '../users/entities/user.entity';
+import { Order } from '../orders/entities/order.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([DriverLocation, User, Order]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>(
+            'JWT_ACCESS_TOKEN_EXPIRATION_TIME',
+          ),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [GeolocationService, DeliveryGateway],
+  exports: [GeolocationService, DeliveryGateway],
+})
+export class GeolocationModule {}
