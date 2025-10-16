2eb81790d262f55d60b9d31255e54a065681a406
José Carlos Pizango Camán <locomancocapac@gmail.com>
2025-10-16 00:45:36 -0500

    background y toggle para dark mode

--- delivery-frontend/src/app/pages/profile/profile.page.ts
+++ delivery-frontend/src/app/pages/profile/profile.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import {
   IonHeader,
   IonToolbar,
@@ -15,16 +16,29 @@ import {
   IonButton,
   IonSpinner,
   IonText,
+  IonList,
+  IonListHeader,
+  IonItem,
+  IonToggle,
+  IonIcon,
 } from '@ionic/angular/standalone';
 import { UserService } from '../../services/user.service';
 import { AuthService } from '../../services/auth.service';
 import type { User } from '../../models/user.model';
+import { addIcons } from 'ionicons';
+import { moonOutline } from 'ionicons/icons';
+
+// Register icons
+addIcons({
+  'moon-outline': moonOutline,
+});
 
 @Component({
   selector: 'app-profile',
   standalone: true,
   imports: [
     CommonModule,
+    FormsModule,
     IonHeader,
     IonToolbar,
     IonButtons,
@@ -39,6 +53,11 @@ import type { User } from '../../models/user.model';
     IonButton,
     IonSpinner,
     IonText,
+    IonList,
+    IonListHeader,
+    IonItem,
+    IonToggle,
+    IonIcon,
   ],
   templateUrl: './profile.page.html',
 })
@@ -47,6 +66,7 @@ export class ProfilePage implements OnInit {
   private auth = inject(AuthService);
   user?: User;
   loading = false;
+  paletteToggle = false;
 
   ngOnInit(): void {
     this.loading = true;
@@ -59,6 +79,34 @@ export class ProfilePage implements OnInit {
         this.loading = false;
       },
     });
+
+    // Use matchMedia to check the user preference
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+
+    // Initialize the dark palette based on the initial
+    // value of the prefers-color-scheme media query
+    this.initializeDarkPalette(prefersDark.matches);
+
+    // Listen for changes to the prefers-color-scheme media query
+    prefersDark.addEventListener('change', (mediaQuery) =>
+      this.initializeDarkPalette(mediaQuery.matches)
+    );
+  }
+
+  // Check/uncheck the toggle and update the palette based on isDark
+  initializeDarkPalette(isDark: boolean) {
+    this.paletteToggle = isDark;
+    this.toggleDarkPalette(isDark);
+  }
+
+  // Listen for the toggle check/uncheck to toggle the dark palette
+  toggleChange(event: CustomEvent) {
+    this.toggleDarkPalette(event.detail.checked);
+  }
+
+  // Add or remove the "ion-palette-dark" class on the html element
+  toggleDarkPalette(shouldAdd: boolean) {
+    document.documentElement.classList.toggle('ion-palette-dark', shouldAdd);
   }
 
   logout() {
